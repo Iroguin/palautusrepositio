4f50322af244eff52f7ce35ce069b8b172653b5e
iroguin <143116756+Iroguin@users.noreply.github.com>
2024-11-11 23:17:49 +0200

    1-3 done

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,15 +1,33 @@
 from urllib import request
 from project import Project
-
+import tomli
+import ssl
 
 class ProjectReader:
     def __init__(self, url):
         self._url = url
 
     def get_project(self):
-        # tiedoston merkkijonomuotoinen sisältö
-        content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        context = ssl._create_unverified_context()
+        
+        content = request.urlopen(self._url, context=context).read().decode("utf-8")
+        
+        toml_data = tomli.loads(content)
+        
+        poetry_data = toml_data["tool"]["poetry"]
+        
+        name = poetry_data.get("name", "")
+        description = poetry_data.get("description", "")
+        license = poetry_data.get("license", "")
+        authors = poetry_data.get("authors", [])
+        dependencies = list(poetry_data.get("dependencies", {}).keys())
+        dev_dependencies = list(poetry_data.get("group", {}).get("dev", {}).get("dependencies", {}).keys())
+        
+        return Project(
+            name,
+            description,
+            license,
+            authors,
+            dependencies,
+            dev_dependencies
+        )
